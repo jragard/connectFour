110363dfb16aff3a5dbc8ba6318bda6ea3742e2c
Ryan <jragard@gmail.com>
2018-02-22 17:56:02 -0500

    added code to ensure that once 6 discs are in 1 column, no more discs may be added

--- main.js
+++ main.js
@@ -1,30 +1,36 @@
-// let board = [
-//     [0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 2, 0],
-//     [0, 0, 0, 0, 1, 1, 1],
-//     [0, 0, 2, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0]
-// ]
+let board = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+]
 
 var redPlayerTurn = true;
 var blackPlayerTurn = false;
+let discCount = 0;
 
-var col1 = document.querySelector("#column1");
+handleClick = function (event) {
 
-col1.onclick = function () {
+    // let discCount = 0;
+
     var redDisc = document.createElement("div");
     redDisc.className = "red";
     var blackDisc = document.createElement("div");
     blackDisc.className = "black";
-    // var destination = document.getElementById("column1");
-    // destination.appendChild(redDisc);
+    
     
     if (redPlayerTurn === true) {
         var destination = document.getElementById("column1");
         destination.appendChild(redDisc);
         redPlayerTurn = false;
         blackPlayerTurn = true;
+        discCount++;
+
+        if (discCount === 6) {
+            col1.removeEventListener('click', handleClick); 
+         }
     }
 
     else {
@@ -32,5 +38,55 @@ col1.onclick = function () {
         destination.appendChild(blackDisc);
         redPlayerTurn = true;
         blackPlayerTurn = false;
+        discCount++;
+
+        if (discCount === 6) {
+            col1.removeEventListener('click', handleClick); 
+         }
     }
+
+    
+
+    // if (discCount === 6) {
+    //     col1.removeEventListener('click', handleClick); 
+    //  }
+
 }
+
+var col1 = document.querySelector("#column1");
+
+col1.addEventListener('click', handleClick);
+
+
+
+// col1.onclick = function () {
+
+//     let discCount = 0;
+
+//     var redDisc = document.createElement("div");
+//     redDisc.className = "red";
+//     var blackDisc = document.createElement("div");
+//     blackDisc.className = "black";
+    
+    
+//     if (redPlayerTurn === true) {
+//         var destination = document.getElementById("column1");
+//         destination.appendChild(redDisc);
+//         redPlayerTurn = false;
+//         blackPlayerTurn = true;
+//         discCount++;
+
+//         if (discCount === 6) {
+            
+//         }
+//     }
+
+//     else {
+//         var destination = document.getElementById("column1");
+//         destination.appendChild(blackDisc);
+//         redPlayerTurn = true;
+//         blackPlayerTurn = false;
+//     }
+
+    
+// }
