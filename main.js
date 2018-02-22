3b23dbd151ca645fe86a79ed4895cb4ce710b473
Ryan <jragard@gmail.com>
2018-02-22 17:30:17 -0500

    toggled player turns. now, for col1, clicking the column will display a red or black disc continually stacking depending on whose turn it is

--- main.js
+++ main.js
@@ -7,12 +7,30 @@
 //     [0, 0, 0, 0, 0, 0, 0]
 // ]
 
+var redPlayerTurn = true;
+var blackPlayerTurn = false;
+
 var col1 = document.querySelector("#column1");
 
 col1.onclick = function () {
     var redDisc = document.createElement("div");
     redDisc.className = "red";
-    var destination = document.getElementById("column1");
-    destination.appendChild(redDisc);
+    var blackDisc = document.createElement("div");
+    blackDisc.className = "black";
+    // var destination = document.getElementById("column1");
+    // destination.appendChild(redDisc);
+    
+    if (redPlayerTurn === true) {
+        var destination = document.getElementById("column1");
+        destination.appendChild(redDisc);
+        redPlayerTurn = false;
+        blackPlayerTurn = true;
+    }
 
+    else {
+        var destination = document.getElementById("column1");
+        destination.appendChild(blackDisc);
+        redPlayerTurn = true;
+        blackPlayerTurn = false;
+    }
 }
